e6e550944eff440bc47daac06b14f1f0ef748012
Huawei Li <huaweil@nvidia.com>
2022-03-27 17:46:58 +0800

    add view plugin

--- wp-content/themes/actione-theme/single.php
+++ wp-content/themes/actione-theme/single.php
@@ -9,7 +9,11 @@
 
     <div class="container container--narrow page-section">
           <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> 
+          <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span>
+          <a><?php echo the_views() ?></a>
+        </p>
       </div>
 
       <div class="generic-content"><?php the_content(); ?></div>
